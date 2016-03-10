a03cdd7cb7fa5194bd4706128f314e75f10698e2
Yohannes Wijaya <readyforservice@icloud.com>
2016-03-10 19:40:15 +0700

    9. UITableView and UITableViewController: Creating the Item Class

---
+++ BNR_Homepwner/Item.swift
@@ -0,0 +1,48 @@
+//
+//  Item.swift
+//  BNR_Homepwner
+//
+//  Created by Yohannes Wijaya on 3/10/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+    
+    init(name: String, valueInDollars: Int, serialNumber: String?) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var randomIdx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(randomIdx)]
+            
+            randomIdx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(randomIdx)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = NSUUID().UUIDString.componentsSeparatedByString("-").first!
+            
+            self.init(name: randomName, valueInDollars: randomValue, serialNumber: randomSerialNumber)
+        }
+        else {
+            self.init(name: "", valueInDollars: 0, serialNumber: nil)
+        }
+    }
+}
+
