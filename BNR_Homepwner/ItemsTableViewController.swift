a76d7e5c5b93548a044d8bd49bd1618787ac7efe
Yohannes Wijaya <readyforservice@icloud.com>
2016-03-14 16:53:46 +0700

    10. Editing UITableView: Editing Mode

--- BNR_Homepwner/ItemsTableViewController.swift
+++ BNR_Homepwner/ItemsTableViewController.swift
@@ -14,70 +14,41 @@ class ItemsTableViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
-    /***
-    var itemsAboveOrEqualTo50 = [Item]()
-    var itemsBelow50 = [Item]()
-    var itemsOfAllPrices = [[Item]]()
-    let sectionHeaderTitles = ["Below $50", "$50 or above"]
-    ***/
+    // MARK: - IBAction Methods
+    
+    @IBAction func addNewItem(sender: UIButton) {
+        
+    }
+    
+    @IBAction func toggleEditingMode(sender: UIButton) {
+        if self.editing {
+            sender.setTitle("Edit", forState: UIControlState.Normal)
+            self.setEditing(false, animated: true)
+        }
+        else {
+            sender.setTitle("Done", forState: .Normal)
+            self.setEditing(true, animated: true)
+        }
+    }
     
     // MARK: - UITableViewDataSource Methods
     
-    /***
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.itemsOfAllPrices.count
-    }
-    ***/
-    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        /***
-        return self.itemsOfAllPrices[section].count
-        ***/
-        
-        return self.itemStore.allItems.count + 1
+        return self.itemStore.allItems.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // Create custom cell just to display "no more item" on the last line
-        if indexPath.row == self.itemStore.allItems.count {
-            let cell = self.tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
-            cell.textLabel?.text = "No more item"
-            cell.detailTextLabel?.text = ""
-            self.tableView.rowHeight = UITableViewAutomaticDimension
-            return cell
-        }
         // Get a new or recycled cell
         let cell = self.tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         // Set the text on the cell w/ the description of the item that is at the nth index of items, where n = row this cell will appear in on the tableview
         let item = self.itemStore.allItems[indexPath.row]
-      
-        /***
-        let item = self.itemsOfAllPrices[indexPath.section][indexPath.row]
-        ***/
         
-//        if let cellBackgroundImage = UIImage(named: "paisley") {
-//            cellBackgroundImage.resizingMode = UIImageResizingMode.Tile
-//            cell.backgroundView = UIImageView(image: backgroundImage)
-//        }
-        
-        if let validImage = UIImage(named: "steps") {
-            cell.backgroundColor = UIColor(patternImage: validImage)
-        }
-        
-        cell.textLabel?.font = UIFont.systemFontOfSize(20)
         cell.textLabel?.text = item.name
-        
-        cell.detailTextLabel?.font = UIFont.systemFontOfSize(20)
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
         return cell
     }
-    
-    /***
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.sectionHeaderTitles[section]
-    }
-    ***/
     
     // MARK: - UIViewController Methods
     
@@ -89,20 +60,5 @@ class ItemsTableViewController: UITableViewController {
         let customInsets = UIEdgeInsets(top: statusBarHeight, left: 0.0, bottom: 0.0, right: 0.0)
         self.tableView.contentInset = customInsets
         self.tableView.scrollIndicatorInsets = customInsets
-        
-        self.tableView.rowHeight = 60
-
-        /***
-        for item in self.itemStore.allItems {
-            if item.valueInDollars >= 50 {
-                self.itemsAboveOrEqualTo50.append(item)
-            }
-            else {
-                self.itemsBelow50.append(item)
-            }
-        }
-        self.itemsOfAllPrices.append(self.itemsBelow50)
-        self.itemsOfAllPrices.append(self.itemsAboveOrEqualTo50)
-        ***/
     }
 }
