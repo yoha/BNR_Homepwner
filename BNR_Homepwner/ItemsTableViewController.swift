5a010d8d48f9c6b6aea388a343177f581d717e7c
Yohannes Wijaya <readyforservice@icloud.com>
2016-03-14 14:14:46 +0700

    9. UITableView and UITableViewController: Bronze Challenge

--- BNR_Homepwner/ItemsTableViewController.swift
+++ BNR_Homepwner/ItemsTableViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ItemsTableViewController: UITableViewController {
+    
+    // MARK: - Stored Properties
+    
     var itemStore: ItemStore!
+    var itemsAboveOrEqualTo50 = [Item]()
+    var itemsBelow50 = [Item]()
+    var itemsOfAllPrices = [[Item]]()
+    let sectionHeaderTitles = ["Below $50", "$50 or above"]
     
     // MARK: - UITableViewDataSource Methods
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.itemsOfAllPrices.count
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.itemStore.allItems.count + 1
+        return self.itemsOfAllPrices[section].count
+//        return self.itemStore.allItems.count + 1
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -29,11 +41,16 @@ class ItemsTableViewController: UITableViewController {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         // Set the text on the cell w/ the description of the item that is at the nth index of items, where n = row this cell will appear in on the tableview
-        let item = self.itemStore.allItems[indexPath.row]
+//        let item = self.itemStore.allItems[indexPath.row]
+        let item = self.itemsOfAllPrices[indexPath.section][indexPath.row]
         
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionHeaderTitles[section]
     }
     
     // MARK: - UIViewController Methods
@@ -46,5 +63,16 @@ class ItemsTableViewController: UITableViewController {
         let customInsets = UIEdgeInsets(top: statusBarHeight, left: 0.0, bottom: 0.0, right: 0.0)
         self.tableView.contentInset = customInsets
         self.tableView.scrollIndicatorInsets = customInsets
+        
+        for item in self.itemStore.allItems {
+            if item.valueInDollars >= 50 {
+                self.itemsAboveOrEqualTo50.append(item)
+            }
+            else {
+                self.itemsBelow50.append(item)
+            }
+        }
+        self.itemsOfAllPrices.append(self.itemsBelow50)
+        self.itemsOfAllPrices.append(self.itemsAboveOrEqualTo50)
     }
 }
