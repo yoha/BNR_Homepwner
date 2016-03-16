b249a88eb501551eccb7aafe865b30f42d104583
Yohannes Wijaya <readyforservice@icloud.com>
2016-03-16 15:52:57 +0700

    11. Subclassing UITableViewCell: Creating ItemCell

---
+++ BNR_Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  BNR_Homepwner
+//
+//  Created by Yohannes Wijaya on 3/16/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
